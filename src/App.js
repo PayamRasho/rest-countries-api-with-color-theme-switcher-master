dae27301b02e979f9bf95bba363581abf68cb0f2
PayamRasho <payamrfattah01@gmail.com>
2024-03-03 22:11:02 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Countries from "./components/Countries";
+import Header from "./components/Header";
+import Filter from "./components/Filter";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 function App() {
   return (
-    <div>hello react</div>
+    <>
+      <Header />
+      <Filter />
+      <Countries />
+    </>
   );
 }
 
