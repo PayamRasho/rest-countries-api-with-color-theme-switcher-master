2b935676c49c28aff0de0f30e8a6ab8095125879
PayamRasho <payamrfattah01@gmail.com>
2024-10-07 18:20:18 +0300

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 const Header = () => {
+const changeTheme =()=>{
+  const moon = document.querySelector('.fa-moon')
+  const header = document.querySelector('.header')
+  moon.addEventListener('click', ()=>{
+    document.body.classList.toggle('light-theme')
+    header.classList.toggle('light-theme')
+  })
+}
   return (
     <>
       <header className="header">
         <div>
           <h1>Where in the world?</h1>
         </div>
-        <div>
-          <i className="fas fa-moon"></i>
-          Dark Mode
-        </div>
+        <i className="fas fa-moon" onClick={changeTheme} ></i>
+      
       </header>
     </>
   );
